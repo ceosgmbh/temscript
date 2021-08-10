1bb251345f04f968b9db1b088c94f87949bdecfd
christian <elting@ceos-gmbh.de>
2021-08-10 15:57:32 +0200

    gun1: Implement Method Call for `GetHighVoltageOffsetRange(double* min, double*max)`
    * can be obtained in python via
    ```
    range = gun1.GetHighVoltageOffsetRange()
    print("HighVoltageOffsetRange=(%s,%s)" % (range[0], range[1]))
    ```
    * is internally calling method `HRESULT raw_GetHighVoltageOffsetRange(&min, &max)` from `StdScript.dll` version > 7.10

--- _temscript_module/gun1.cpp
+++ _temscript_module/gun1.cpp
@@ -5,12 +5,27 @@
 DOUBLE_PROPERTY_GETTER(Gun1, HighVoltageOffset)
 DOUBLE_PROPERTY_SETTER(Gun1, HighVoltageOffset)
 
+static PyObject* Gun1_GetHighVoltageOffsetRange(Gun1 *self)
+{
+    double min, max;
+    HRESULT result = self->iface->raw_GetHighVoltageOffsetRange(&min, &max);
+    if (FAILED(result)) {
+        raiseComError(result);
+        return NULL;
+    }
+
+    return Py_BuildValue("dd", min, max);
+}
+
 static PyGetSetDef Gun1_getset[] = {
-    {"HighVoltageOffset",  (getter)&Gun1_get_HighVoltageOffset, (setter)&Gun1_set_HighVoltageOffset, NULL, NULL},
-    //{"GetHighVoltageOffsetRange",      (getter)&Gun1_get_HighVoltageOffsetRange, NULL, NULL, NULL},
+    {"HighVoltageOffset",         (getter)&Gun1_get_HighVoltageOffset, (setter)&Gun1_set_HighVoltageOffset, NULL, NULL},
     {NULL}  /* Sentinel */
 };
 
-IMPLEMENT_WRAPPER(Gun1, TEMScripting::Gun1, Gun1_getset, 0)
-// IMPLEMENT_WRAPPER(Gun1, TEMScripting::Gun, Gun1_getset, 0)
-// IMPLEMENT_WRAPPER2(Gun1, TEMScripting::Gun, Gun1_getset, 0)
+static PyMethodDef Gun1_methods[] = {
+    {"GetHighVoltageOffsetRange",      (PyCFunction)Gun1_GetHighVoltageOffsetRange, METH_NOARGS, NULL},
+    {NULL}  /* Sentinel */
+};
+
+
+IMPLEMENT_WRAPPER(Gun1, TEMScripting::Gun1, Gun1_getset, Gun1_methods)
